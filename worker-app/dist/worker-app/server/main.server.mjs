d81b5c4eed267b504f75b3ed63bca5bcf0bd31d5
JoseDonadoManuel <jose.hernandez@dreamcodesoft.com>
2026-07-31 17:18:30 -0500

    chore: ignore compiled dist output and untrack worker-app/dist

--- worker-app/dist/worker-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-PYIKYXRB.mjs";import"./chunk-YJGPOIYT.mjs";import"./chunk-T3PSMHJ6.mjs";import"./chunk-DWOH5EYG.mjs";import"./chunk-RRGH7JLE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
